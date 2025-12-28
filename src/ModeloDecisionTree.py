86f543a7e9573fb8709abe2a06fd9ab3ef4ce732
Matheuszy <matheusalmeidacarlos@gmail.com>
2025-12-28 17:12:12 -0300

    criando ModeloDecisionTree.py

---
+++ src/ModeloDecisionTree.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+class ModeloDecisionTree:
+    def __init__(self):
+        pass
+
+    def gerando_resultado_final(self, model, x_test):
+        # Cria a base com as previsões (0 ou 1)
+        resultado_final = x_test.copy()
+        resultado_final['Previsao_Churn'] = model.predict(x_test)
+
+        # Salva o CSV
+        resultado_final.to_csv('base_com_previsoes.csv', index=False)
+        print("Arquivo base_com_previsoes.csv gerado!")
